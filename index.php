70b48c2dfb7ff513dc87e901d64dd3ec5d21bc0f
Clems-web <clement.servais1@gmail.com>
2021-01-27 13:20:24 +0100

    finito !

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 
 // Premiere ligne
 
-
+echo date('l jS \of F Y')."<br>";
+//
+$time = strtotime("10/12/2018");
+echo date("Y.d.m", $time)."<br>";
+//
+$hours = strtotime("11:35:07");
+echo date("H:i:s", $hours);
 
 
 
